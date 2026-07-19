4f82b131eb0db8e50477f356c322658aebe097b9
Bhasvic-ESmith <ethan.smith25@students.bhasvic.ac.uk>
2026-07-19 23:03:07 +0100

    layout begun

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+score = []
 
 @app.route('/')
 def hello():
-    return "<h1>This is my project</h1>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
